3b87aed0e301113ce1999795fbe1610d074734a6
cma0022 <cma0022@alu.medac.es>
2023-04-12 19:26:13 +0200

    TercerCommit

--- optimizar.py
+++ optimizar.py
@@ -1,23 +1,27 @@
 class optimizar:
 
-    array=[]
+    # Creación de una varible tipo array
+    array = []
 
+    # Constructor con parámetro
     def __init__(self,array):
         self.array = array
 
+    # Método que imprime "introduzca un numero", escanea 
+    # por teclado y devuelvo el num introducido
     def input_numero(self) -> int:
         print("Introduzca un numero:")
         n = int(input())
         return n
-
+    
+    # Método para añadir un num que pasan por parametro al 
+    # array y enviar un mensaje de aviso
     def anhadir_array(self, n):
         self.array.append(n)
         print(f"Numero {n} introducido en el array.")
 
-    def uso_metodo(self):
-        for _ in range (3):
-            self.introducir_numero()
-
+    # Método para sumar los números del array, hacer su media, 
+    # imprimirlo por pantalla y devolverlo
     def sumar_mostrar(self) -> int:
         suma = 0
         for n in self.array:
--- test_optimizar.py
+++ test_optimizar.py
@@ -1,5 +1,10 @@
 from optimizar import optimizar
 
+# La cobertura de código no es del 100% debido a que uno de los método
+# utiliza un input
+
+# Método tipo test. Crea un objeto de la clase optimizar, añade 5 
+# numeros al array y se comprueba la longitud de este
 def test_anhadir_array():
     op = optimizar([])
     op.anhadir_array(5)
@@ -9,6 +14,8 @@ def test_anhadir_array():
     op.anhadir_array(2)
     assert len(op.array) == 5
 
+# Método tipo test. Crea un objeto de la clase optimizar con 5 valores
+#  en el array y se comprueba que la media de estos valores sea 4
 def test_sumar_mostrar():
     op = optimizar([6, 4, 2, 8, 0])
     assert op.sumar_mostrar() == 4
